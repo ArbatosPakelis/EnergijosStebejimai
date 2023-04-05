555b0314e03a2db0000b0d3584ca57214400d9f2
matas108 <m.dzingulevicius@gmail.com>
2023-04-05 12:03:22 +0300

    Revert "Update dayApi.js"
    This reverts commit 9ec5b4fb912afca3d6bd92a3606511a877ce88de.

--- frontend/src/Apis/dayApi.js
+++ frontend/src/Apis/dayApi.js
@@ -4,7 +4,6 @@ export default class dayApi extends Api {
   getAll = () => {
     return super.init().get(`getAllDays`);
   };
-  // siapsau
   // get = () => {
   //   return super.init().get(`getDay/:id`);
   // };
