9ec5b4fb912afca3d6bd92a3606511a877ce88de
matas108 <m.dzingulevicius@gmail.com>
2023-04-05 11:44:06 +0300

    Update dayApi.js

--- frontend/src/Apis/dayApi.js
+++ frontend/src/Apis/dayApi.js
@@ -4,6 +4,7 @@ export default class dayApi extends Api {
   getAll = () => {
     return super.init().get(`getAllDays`);
   };
+  // siapsau
   // get = () => {
   //   return super.init().get(`getDay/:id`);
   // };
